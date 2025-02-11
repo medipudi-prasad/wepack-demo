c3545e0bddd69b504bd6f53163396fe0f7492329
Prasad Medipudi <medipudi.prasad@gmail.com>
2025-02-11 12:17:33 +0530

    Add prod and dev configs, dev-server - Broke our webpack.config file into 3 files - webpack.common.js, webpack.dev.js, and webpack.prod.js - installed webpack-merge to share the common functionality - updated our package.json to use the new config files - installed webpack-dev-server and added it to start script in package.json

---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+});
+
+//in Package.json webpack-dev-server should not upgrade if it upgrades then open will not work
